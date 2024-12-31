16fd736599ae7b9cc1f6b0a340d7fad3f55ba226
Daniele Solombrino <dansolombrino@gmail.com>
2024-12-31 16:20:00 +0100

    add 1 epoch of gd vs. 1 epoch of sgd comparisons

---
+++ acc_grad_batches_data_lightning.py
@@ -0,0 +1,96 @@
+CIFAR100_acc_grad_batches_0001_epochs_1_optim_SGD = 0.7594000101089478
+CIFAR100_acc_grad_batches_1407_epochs_1_optim_SGD = 0.6725999712944031
+CIFAR100_acc_grad_batches_1407_epochs_1_optim_SGD = 0.6725999712944031 # adjusted lr, div by num batches
+CIFAR100_acc_grad_batches_1407_epochs_1_optim_SGD = 0.6723999977111816 # adjusted lr, mult by num batches
+CIFAR100_acc_grad_batches_1407_epochs_1_optim_SGD = 0.6729999780654907 # loss reduction = sum
+CIFAR_100_zs                                      = 0.6719999909400940
+CIFAR100_acc_grad_batches_1407_epochs_1_optim_SGD = 0.6756000000000000 # custom train loop, adjusted lr, div by num batches 
+
+
+################################################################################
+
+DTD_acc_grad_batches_0001_epochs_01_optim_SGD = 0.49223947525024414
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.49002218246459960
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.49002218246459960 # adjusted lr, div by num batches
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.49002218246459960 # adjusted lr, mult by num batches
+DTD_zs                                        = 0.48337027430534363
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.49002217294900224 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+EuroSAT_acc_grad_batches_0001_epochs_1_optim_SGD = 0.8751388788223267
+EuroSAT_acc_grad_batches_0675_epochs_1_optim_SGD = 0.5409722328186035
+EuroSAT_acc_grad_batches_0675_epochs_1_optim_SGD = 0.5406481623649597 # adjusted lr, mult by num batches
+EuroSAT_acc_grad_batches_0675_epochs_1_optim_SGD = 0.5406481623649597 # adjusted lr, div by num batches
+EuroSAT_zs                                       = 0.5395833253860474
+EuroSAT_acc_grad_batches_0675_epochs_1_optim_SGD = 0.5406481481481481 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+GTSRB_acc_grad_batches_0001_epochs_1_optim_SGD = 0.5762012004852295
+GTSRB_acc_grad_batches_0750_epochs_1_optim_SGD = 0.4185435473918915
+GTSRB_acc_grad_batches_0750_epochs_1_optim_SGD = 0.4185435473918915 # adjusted lr, mult by num batches
+GTSRB_acc_grad_batches_0750_epochs_1_optim_SGD = 0.4185435473918915 # adjusted lr, div by num batches
+GTSRB_zs                                       = 0.42417415976524353
+GTSRB_acc_grad_batches_0750_epochs_1_optim_SGD = 0.41854354354354356 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+MNIST_acc_grad_batches_0001_epochs_1_optim_SGD = 0.9002000093460083
+MNIST_acc_grad_batches_1719_epochs_1_optim_SGD = 0.5102000236511230
+MNIST_acc_grad_batches_1719_epochs_1_optim_SGD = 0.5099999904632568
+MNIST_zs                                       = 0.5149999856948853
+MNIST_acc_grad_batches_1719_epochs_1_optim_SGD = 0.5100000000000000 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+RESISC45_acc_grad_batches_0001_epochs_1_optim_SGD = 0.7195767164230347
+RESISC45_acc_grad_batches_0532_epochs_1_optim_SGD = 0.6656084656715393
+RESISC45_acc_grad_batches_0532_epochs_1_optim_SGD = 0.6645502448081970 # adjusted lr
+RESISC45_zs                                       = 0.6708994507789612
+RESISC45_acc_grad_batches_0532_epochs_1_optim_SGD = 0.6645502645502646 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+SVHN_acc_grad_batches_0001_epochs_1_optim_SGD = 0.78560000658035280
+SVHN_acc_grad_batches_2134_epochs_1_optim_SGD = 0.44800001382827760
+SVHN_zs                                       = 0.44859999418258667
+SVHN_acc_grad_batches_2134_epochs_1_optim_SGD = 0.44800000000000000 # custom train loop, adjusted lr, div by num batches
+
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DTD_acc_grad_batches_0001_epochs_01_optim_SGD = 0.49223947525024414
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.4900221824645996
+DTD_acc_grad_batches_0127_epochs_01_optim_SGD = 0.4900221824645996 # adjusted lr
+
+
+DTD_acc_grad_batches_0001_epochs_5_optim_SGD = 0.516629695892334
+DTD_acc_grad_batches_0127_epochs_5_optim_SGD = 0.4811529815196991
+DTD_acc_grad_batches_0127_epochs_5_optim_SGD = 0.4811529815196991 # adjusted lr
+
+################################################################################
+
+SVHN_acc_grad_batches_001_epochs_4_optim_SGD = 0.8899999856948853
+SVHN_acc_grad_batches_001_epochs_4_optim_SGD = 0.8899999856948853 # using max_steps = max_epochs * len(dataset.train_loader)
+
+SVHN_acc_grad_batches_2134_epochs_4_optim_SGD = 0.4519999921321869
+SVHN_acc_grad_batches_2134_epochs_4_optim_SGD = 0.45100000500679016 # adjusted lr
+SVHN_acc_grad_batches_2134_epochs_4_optim_SGD = -6969 # adjusted lr, using max_steps = max_epochs * len(dataset.train_loader)
+
---
+++ acc_grad_batches_data_manual_loop_manual_optim.py
@@ -0,0 +1,68 @@
+CIFAR100_zs = 0.67200000
+CIFAR100_0_batch_size_032_lim_train_batches_1407_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.78660000
+CIFAR100_0_batch_size_032_lim_train_batches_1407_acc_grad_batches_1407_epochs_1_optim_SGD_order_1 = 0.67180000
+CIFAR100_0_batch_size_032_lim_train_batches_1407_acc_grad_batches_1407_epochs_1_optim_SGD_order_1 = 0.67180000 # grad / num_batches
+CIFAR100_0_batch_size_032_lim_train_batches_1407_acc_grad_batches_1407_epochs_1_optim_SGD_order_1 = 0.67180000 # lr * num_batches
+CIFAR100_0_batch_size_032_lim_train_batches_1407_acc_grad_batches_1407_epochs_1_optim_SGD_order_1 = 0.67180000 # lr / num_batches
+
+################################################################################
+
+DTD_zs = 0.48115299
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.50554324
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0127_epochs_1_optim_SGD_order_1 = 0.49002217
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0127_epochs_1_optim_SGD_order_1 = 0.48780488 # grad / num_batches
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0127_epochs_1_optim_SGD_order_1 = 0.50997783 # grad * num_batches
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.50554324 # lr * num_batches
+DTD_0_batch_size_032_lim_train_batches_0127_acc_grad_batches_0127_epochs_1_optim_SGD_order_1 = 0.48780488 # lr / num_batches
+
+################################################################################
+
+EuroSAT_zs = 0.53833333
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.88143519
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0675_epochs_1_optim_SGD_order_1 = 0.54060185
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0675_epochs_1_optim_SGD_order_1 = 0.54004630 # grad / num_batches
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0675_epochs_1_optim_SGD_order_1 = 0.55888889 # grad * num_batches
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0675_epochs_1_optim_SGD_order_1 = 0.55847222 # lr * num_batches
+EuroSAT_0_batch_size_032_lim_train_batches_0675_acc_grad_batches_0675_epochs_1_optim_SGD_order_1 = 0.54004630 # lr / num_batches
+
+################################################################################
+
+GTSRB_zs = 0.42342342
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.55555556
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0750_epochs_1_optim_SGD_order_1 = 0.41891892
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0750_epochs_1_optim_SGD_order_1 = 0.41816817 # grad / num_batches
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0750_epochs_1_optim_SGD_order_1 = 0.02252252 # grad * num_batches
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0750_epochs_1_optim_SGD_order_1 = 0.02252252 # lr * num_batches
+GTSRB_0_batch_size_032_lim_train_batches_0750_acc_grad_batches_0750_epochs_1_optim_SGD_order_1 = 0.41816817 # lr / num_batches
+
+################################################################################
+
+MNIST_zs = 0.52120000
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.72280000
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_1719_epochs_1_optim_SGD_order_1 = 0.51600000
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_1719_epochs_1_optim_SGD_order_1 = 0.51600000 # grad / num_batches
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_1719_epochs_1_optim_SGD_order_1 = 0.51600000 # grad * num_batches
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_1719_epochs_1_optim_SGD_order_1 = 0.51600000 # lr * num_batches
+MNIST_0_batch_size_032_lim_train_batches_1719_acc_grad_batches_1719_epochs_1_optim_SGD_order_1 = 0.51600000 # lr / num_batches
+
+################################################################################
+
+RESISC45_zs = 0.67301587
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.74550265
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0532_epochs_1_optim_SGD_order_1 = 0.66455026
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0532_epochs_1_optim_SGD_order_1 = 0.66455026 # grad / num_batches
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0532_epochs_1_optim_SGD_order_1 = 0.05873016 # grad * num_batches
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0532_epochs_1_optim_SGD_order_1 = 0.05873016 # lr * num_batches
+RESISC45_0_batch_size_032_lim_train_batches_0532_acc_grad_batches_0532_epochs_1_optim_SGD_order_1 = 0.66455026 # lr / num_batches
+
+################################################################################
+
+SVHN_zs = 0.45060000
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_0001_epochs_1_optim_SGD_order_1 = 0.83720000
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_2134_epochs_1_optim_SGD_order_1 = 0.44840000
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_2134_epochs_1_optim_SGD_order_1 = 0.45000000 # grad / num_batches
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_2134_epochs_1_optim_SGD_order_1 = 0.14320000 # grad * num_batches
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_2134_epochs_1_optim_SGD_order_1 = 0.14320000 # lr * num_batches
+SVHN_0_batch_size_032_lim_train_batches_2134_acc_grad_batches_2134_epochs_1_optim_SGD_order_1 = 0.45000000 # lr / num_batches
+
+################################################################################
