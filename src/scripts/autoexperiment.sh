d4b9c4d6170a2ff7167641932a8d9f451b0c6340
Daniele Solombrino <dansolombrino@gmail.com>
2025-01-03 15:46:20 +0100

    update folder storing autoexperiment terminal logs

--- src/scripts/autoexperiment.sh
+++ src/scripts/autoexperiment.sh
@@ -6,7 +6,7 @@ if [[ "$1" == "--timestamp" ]]; then
     timestamp=$(python3 -c "from datetime import datetime; print(datetime.now().strftime('%Y_%m_%d_at_%H_%M_%S'))")
     
     # Run with timestamp
-    python3 src/scripts/autoexperiment.py --timestamp=$timestamp --called-from-bash-script=True | tee ./run_terminal_logs/autoexperiment_output_log_$timestamp.log
+    python3 src/scripts/autoexperiment.py --timestamp=$timestamp --called-from-bash-script=True | tee ./run_terminal_logs/autoexperiment/autoexperiment_output_log_$timestamp.log
 else
     # Run without timestamp, explicitly passing "None"
     python3 src/scripts/autoexperiment.py --timestamp=None --called-from-bash-script=True
